0fe4e2e98e227b81aab7b8c4f4d935fba641e335
yerimbet.aitzhanov <king_erosh@icloud.com>
2024-10-07 20:40:14 +0500

    Initial Commit

---
+++ MovieDB1Tests/MovieDB1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDB1Tests.swift
+//  MovieDB1Tests
+//
+//  Created by Ерош Айтжанов on 07.10.2024.
+//
+
+import Testing
+@testable import MovieDB1
+
+struct MovieDB1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
